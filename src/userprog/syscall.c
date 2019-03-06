bd1f8c328b3d63e6be883006d35534c25141a742
Chineye Emeghara <chineye@cs.utexas.edu>
2019-03-06 15:58:12 -0600

    not alot:
     Please enter the commit message for your changes. Lines starting

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -7,14 +7,33 @@
 static void syscall_handler (struct intr_frame *);
 
 void
-syscall_init (void) 
+syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f UNUSED)
 {
+
+    if(*(f->esp) == SYS_HALT){
+      halt();
+    }
+
+
   printf ("system call!\n");
   thread_exit ();
+}
+
+/*  */
+void halt(void){
+
+  shutdown_power_off();
+
+}
+
+void exit(int status){
+
+  return status;
+
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -2,5 +2,31 @@
 #define USERPROG_SYSCALL_H
 
 void syscall_init (void);
+ /* terminates Pintos by calling shutdown_power_off()*/
+void halt(void);
+/*terminates the current user program*/
+void exit(int status);
+/*runs the executable whos name is given by cmd_line*/
+pid_t exec(const char *cmd_line);
+
+int wait(pid_t pid);
+
+bool create(const char *file, unsigned intial_size);
+
+bool remove(const char *file);
+
+int open(const char *file);
+
+int filesize(int fd);
+
+int read(int fd, void *buffer, unsigned size);
+
+int write(int fd, const void *buffer, unsigned size);
+
+void seek(int fd, unsigned position);
+
+unsigned tell(int fd);
+
+void close(int fd);
 
 #endif /* userprog/syscall.h */
