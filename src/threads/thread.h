b2fb29c709d61cd35ee074601c295c2ffbcef2e5
Chineye Emeghara <chineye@cs.utexas.edu>
2019-04-19 18:08:43 -0500

    Pushing thread.c `

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -115,8 +115,8 @@ struct thread
 
     struct list_elem child_elem; //elem for children list
     struct list children; // list of children the thread has
-    int stack_pages;
-    struct hash spt;      //supplemental page table
+    int stack_pages; /* stack pages of the thread*/
+    struct hash spt;  //supplemental page table
 
 
 
